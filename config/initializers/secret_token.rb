d4148896a84c5dfc95727f31b31f6f9e51a09e4b
Tara Lerias <tlerias@gmail.com>
2014-06-02 12:14:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LinkerApp::Application.config.secret_key_base = 'e9283dc9a79877ce2431890c3cf3a0d53af571d22865432b5410b738d95569802bfc33e94c1227fe15a77858d07dd0a5f49d4c2e95ab47b4cd7546d74ef6778e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LinkerApp::Application.config.session_store :cookie_store, key: '_linker_app_session'
